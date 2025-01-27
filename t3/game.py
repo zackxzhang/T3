d3e486f1f7695df959df05a40ad8a2b954746169
Zack Zhang <zack.zhang.coder@gmail.com>
2025-01-27 18:26:35 -0500

    revise transition and action

--- t3/game.py
+++ t3/game.py
@@ -1,6 +1,6 @@
-from .state import state_0, boardify, statify, stringify, victor, impasse
+from .state import state_0, boardify, stringify, judge
 from .player import Player
-from .hint import Stone, State
+from .hint import Stone, State, Board
 
 
 class Game:
@@ -9,33 +9,26 @@ class Game:
         for player in players:
             player.state = state_0
         self.players = players
-        self.board = boardify(state_0)
-        self.prev = players[1].stone
+        self.state = state_0
         self.round = 0
         self.winner = Stone._
         self.history: list = list()
-
-    def __str__(self) -> str:
-        return stringify(self.board)
 
     @property
     def player(self) -> Player:
         return self.players[self.round % 2]
 
     @property
-    def state(self) -> State:
-        return statify(self.board)
+    def board(self) -> Board:
+        return boardify(self.state)
 
-    def evo(self, action):
+    def __str__(self) -> str:
+        return stringify(self.board)
+
+    def evo(self, action: State):
         assert not self.winner, "game is ended"
         self.history.append(action)
-        stone, (i, j) = action
-        assert self.prev == ~stone
-        self.prev = stone
-        assert not self.board[i][j], f"{i}-{j} is occupied"
-        self.board[i][j] = stone
+        self.winner = judge(action)
+        self.state = action
         self.round += 1
-        self.winner = victor(self.board)
-        if impasse(self.board):
-            self.winner = Stone.X | Stone.O
         return self.winner
