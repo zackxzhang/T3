94fb9f7330457d1a59b8ae708e72fbe5bf13ee97
Zack Zhang <zack.zhang.coder@gmail.com>
2025-02-04 18:38:07 -0500

    add thread-safe structs

--- t3/value.py
+++ t3/value.py
@@ -1,9 +1,41 @@
-from .struct import Value
+import threading
 from .state import states, encode_state, decode_state
 
 
-def init_value(constant=0., random=False):
-    return {s: constant for s in states}
+class Value(dict):
+
+    def __init__(self, *args, k=4, **kwargs):
+        self.k = k
+        self.loc = threading.Lock()
+        self.sem = threading.Semaphore(k)
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, key):
+        with self.loc:
+            pass
+        self.sem.acquire()
+        try:
+            return super().__getitem__(key)
+        finally:
+            self.sem.release()
+
+    def __setitem__(self, key, value):
+        with self.loc:
+            for _ in range(self.k):
+                self.sem.acquire()
+        try:
+            super().__setitem__(key, value)
+        finally:
+            for _ in range(self.k):
+                self.sem.release()
+
+    def items(self):
+        with self.loc:
+            return super().items()
+
+
+def init_value(constant: float = 0., random: bool = False) -> Value:
+    return Value((state, constant) for state in states)
 
 
 def encode_value(value: Value) -> dict[str, float]:
@@ -11,4 +43,4 @@ def encode_value(value: Value) -> dict[str, float]:
 
 
 def decode_value(json: dict[str, float]) -> Value:
-    return {decode_state(s): v for s, v in json.items()}
+    return Value((decode_state(s), v) for s, v in json.items())
